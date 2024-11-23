7dc9b791e07555ed3a91f2c28066dc38f711096a
unknown <moisesgonzalespedraza9@gmail.com>
2024-11-23 09:47:15 -0500

    avance 6

--- miproyectored/src/app/login/login.component.ts
+++ miproyectored/src/app/login/login.component.ts
@@ -1,24 +1,29 @@
-import { Component } from '@angular/core';
-import { RegisterComponent } from '../register/register.component';
-import { RouterLink, RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormBuilder, FormControl, FormsModule, Validators, } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule } from '@angular/forms';
+ interface formlogin {
+  email: FormControl<string | null>;
+  password: FormControl<string | null>;
+ }
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  imports: [FormsModule, RouterModule, CommonModule]
+  imports: [FormsModule, RouterModule, CommonModule,ReactiveFormsModule]
   
 })
 export class LoginComponent {
-  email: string = '';
-  password: string = '';
+  private _formBuilder = inject(FormBuilder);
+    form = this._formBuilder.group<formlogin>({
+    email: this._formBuilder.control('',[Validators.required, Validators.email]),
+    password: this._formBuilder.control('',[Validators.required]),
+  });
+  Submit(): void {
+    console.log(this.form.getRawValue());
 
-  onSubmit(): void {
-    console.log('Email:', this.email);
-    console.log('Password:', this.password);
   }
 }
--- miproyectored/src/app/register/register.component.ts
+++ miproyectored/src/app/register/register.component.ts
@@ -1,33 +1,53 @@
 import { LetDeclaration } from '@angular/compiler';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+interface formregister{
+  name: FormControl<string | null>;
+  apell: FormControl<string | null>;
+  email: FormControl<string | null>;
+  password: FormControl<string | null>;
+  confirmpassword: FormControl<string | null>;
+}
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule, RouterModule],
+  imports: [FormsModule, RouterModule, ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
   
 })
 export class RegisterComponent {
-  name: string = '';
-  apell: string = '';
-  email: string = '';
-  password: string = '';
-  confirmPassword: string = '';
+  private _formBuilder = inject(FormBuilder);
+    form = this._formBuilder.group<formregister>({
+    name: this._formBuilder.control('',[Validators.required]),
+    apell: this._formBuilder.control('',[Validators.required]),
+    email: this._formBuilder.control('',[Validators.required, Validators.email]),
+    password: this._formBuilder.control('',[Validators.required]),
+    confirmpassword: this._formBuilder.control('',[Validators.required])
+      
+  });
+  errorMessage: string = '';
+  showPassword: boolean = false; 
+  showConfirmPassword: boolean = false; 
 
-  onRegister(): void {
-    if (this.password === this.confirmPassword){
-      console.log('name:', this.name);
-    console.log('email:', this.email);
-    console.log('confirmPassword:', this.confirmPassword);
-    console.log('Password:', this.password);
-    }else {
-      console.log('las contraseñas no coinciden');
+  Submit(): void {
+    if(this.form.invalid) return;
+     const { name, apell, email, password, confirmpassword }= this.form.value;
+     if (!email || !password || !name || !apell || !confirmpassword) return;
+     if (password !== confirmpassword) {
+      this.errorMessage = 'Las contraseñas no coinciden';
+      return;
     }
-    
-  } 
-}
+
+    this.errorMessage = '';
+     console.log({ name, apell, email, password, confirmpassword});
+     
+    }
+
+  }
+
 
